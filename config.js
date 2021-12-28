2066db82a20f8ce0df8c3bdf5873a8846b98a7c9
Nandadewanta <94246636+Nandadewanta@users.noreply.github.com>
2021-12-29 04:22:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,24 +2,24 @@ global.DeveloperMode = 'false' //true Or false
 global.linkGC = ['https://chat.whatsapp.com/Lb4Emjih98rBiCZiZoS2eM', 'https://chat.whatsapp.com/FrluVyY5kBRIhY6yKfH4DI']
 global.Owner = {
   // use the phone number with the country code, for example Indonesia '62' America '1' etc
-  '62857139649630': { // put your number here
+  '62857974339563': { // put your number here
     name: 'Owner Metro Bot/Creator games-wabot',
     isDev: false, // if true this number will send if bot have bug and developer mode true
     isReport: false, // if true this number will send if user use command /report or /join
     isCreator: true // if true this number will send if user use command /owner or /creator
     },
-  '6281998903280': { // put your number here
-  name: 'KyuraSenpai/laucher bot',
+  '62857974339563': { // put your number here
+  name: 'DewantZbot',
     isDev: true, // if true this number will send if bot have bug and developer mode true
     isReport: true, // if true this number will send if user use command /report or /join
     isCreator: false // if true this number will send if user use command /owner or /creator
   },
-  '6281390658325': { // put your number here
+  '62857974339563': { // put your number here
     isDev: true, // if true this number will send if bot have bug and developer mode true
     isReport: true, // if true this number will send if user use command /report or /join
     isCreator: false // if true this number will send if user use command /owner or /creator
   },
-  '6282256115584': { // put your number here
+  '62857974339563': { // put your number here
     name: 'Dave',
     isDev: false, // if true this number will send if bot have bug and developer mode true
     isReport: true, // if true this number will send if user use command /report or /join
@@ -60,7 +60,7 @@ global.APIKeys = { // APIKey Here
 
 // Sticker WM
 global.packname = 'From'
-global.author = 'Kyura BOT'
+global.author = 'DewantaPolos'
 
 global.multiplier = 70 // The higher, The harder levelup
 
